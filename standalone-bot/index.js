834ed721d52da984b0b82d1758c03a60f5fcdb54
Josh <66495148+Joshwgaming@users.noreply.github.com>
2022-03-05 14:39:55 +0000

    Update index.js

--- standalone-bot/index.js
+++ standalone-bot/index.js
@@ -1,1 +1,13 @@
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const { botToken } = require('./config.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+client.login(botToken);
+
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}`);
+});
+
+
+const badLinksAutoMod = require('./automod/badLinks.js');
+badLinksAutoMod(client, Client, MessageEmbed)
